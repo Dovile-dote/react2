baf034138662a22e397766541b68c29263e0ee08
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-05-21 14:08:35 +0300

    react base list

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@
 import './App.css';
 const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
 const dogs1 =[...dogs];
-dogs1.sort((a, b) => b.length - a.length)
+dogs1.sort((a, b) => b.length - a.length);
+
+let dogs2 = [];
+for (let i = 0; i < dogs.length; i++) {
+  if (dogs[i][0] !== dogs[i][0].toUpperCase()) {
+    // dogs2.push(dogs[i]);
+    dogs2 =[...dogs2, dogs[i]]; 
+  }
+  
+}
 
 function App() {
   return (
@@ -29,17 +38,31 @@ function App() {
         {/* Atvaizduoti masyvą dogs. Šunis, kurie prasideda didžiąja raide praleisti (neatvaizduoti). */}
         <section>
           {
-            dogs.map((dog, i) => dog[0] === dog[0].toUpperCase ? <div className="apskritimas" key={i}>dog</div>:<div className='kvadratas' key={i}></div>)
-            // dogs.map((dog, i) =>  <div className="apskritimas" key={i} > {dog[0] === dog[0].toUpperCase ? i : dog}</div>)
+            dogs.map((dog, i) => dog[0] !== dog[0].toUpperCase() ? <div className="apskritimas" key={i}>{dog}</div>:<div key={i}></div>)
           }
-          <p>nepabaigtas, nesugalvoju kaip isrinkti su mazosiom raidem</p>
         </section>
-          {/* Naudojant masyvą dogs atvaizduoti skaičius, kurie yra lygūs žodžių masyve ilgiui. Skaičius, didesnius nei 6 atvaizduoti žaliai, kitus raudonai. */}
-          <section>
-            {
-              dogs.map((dog, i) => <div key={i} className='number' style={{color:dog.length > 6 ? "green" : "red", boxShadow:dog.length > 6 ? "0 0 15px green" :"0 0 15px red" }}>{dog.length}</div>)
-            }
-          </section>
+
+        <section>
+          {
+            dogs2.map((dog, i) => <div className="suo" key={i}>
+              <div className="kaire-ausis"></div>
+              <div className="snukis">
+                <div className="akys">
+                  <div className='rainele'><div className='vizdys'></div></div>
+                  <div className='rainele'><div className='vizdys'></div></div>
+                </div>
+                <p>{dog}</p>
+              </div>
+              <div className="desine-ausis"></div>
+            </div>)
+          }
+        </section>
+        {/* Naudojant masyvą dogs atvaizduoti skaičius, kurie yra lygūs žodžių masyve ilgiui. Skaičius, didesnius nei 6 atvaizduoti žaliai, kitus raudonai. */}
+        <section>
+          {
+            dogs.map((dog, i) => <div key={i} className='number' style={{color:dog.length > 6 ? "green" : "red", boxShadow:dog.length > 6 ? "0 0 15px green" :"0 0 15px red" }}>{dog.length}</div>)
+          }
+        </section>
       </header>
     </div>
   );
