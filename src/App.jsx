be9ad6226cdee14823507a2af3a7b45f1b582cf2
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-05-26 17:08:24 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-
 import './App.css';
 const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
-const dogs1 =[...dogs];
+const dogs1 = [...dogs];
 dogs1.sort((a, b) => b.length - a.length);
 
 let dogs2 = [];
 for (let i = 0; i < dogs.length; i++) {
   if (dogs[i][0] !== dogs[i][0].toUpperCase()) {
-    // dogs2.push(dogs[i]);
-    dogs2 =[...dogs2, dogs[i]]; 
+    dogs2 = [...dogs2, dogs[i]];
   }
-  
 }
 
 function App() {
@@ -19,49 +16,66 @@ function App() {
       <header className="App-header">
         {/* Atvaizduoti masyvą dogs. Kiekvienas šuo atskirame kvadrate */}
         <section>
-          {
-            dogs.map((k, i) => <div className="kvadratas" key={i}>{k}</div>)
-          }
+          {dogs.map((dog, i) => (
+            <div className="kvadratas" key={i}>
+              {dog}
+            </div>
+          ))}
         </section>
         {/*  Atvaizduoti masyvą dogs. Kiekvienas šuo atskirame apskritime. Šunys turi būti išrūšiuoti nuo ilgiausio žodžio iki trumpiausio, o apskritimai sunumeruoti nuo 1 iki galo. */}
         <section>
-          {
-            dogs1.map((k, i) => <div className="apskritimas" key={i}> <span>{i+1}</span>{k}</div>)
-          }
+          {dogs1.map((dog, i) => (
+            <div className="apskritimas" key={i}>
+              <span>{i + 1}</span>
+              {dog}
+            </div>
+          ))}
         </section>
         {/* Atvaizduoti masyvą dogs. Poriniuose indeksuose esančius šunis atvaizduoti kvadratuose, neporinius apskritime. */}
         <section>
-          {
-            dogs.map((dog, i) => <div key={i} className={i % 2 === 0 ? "kvadratas":"apskritimas"}>{dog}</div>) 
-          }
+          {dogs.map((dog, i) => (
+            <div key={i} className={i % 2 === 0 ? 'kvadratas' : 'apskritimas'}>
+              {dog}
+            </div>
+          ))}
         </section>
         {/* Atvaizduoti masyvą dogs. Šunis, kurie prasideda didžiąja raide praleisti (neatvaizduoti). */}
         <section>
-          {
-            dogs.map((dog, i) => dog[0] !== dog[0].toUpperCase() ? <div className="apskritimas" key={i}>{dog}</div>:<div key={i}></div>)
-          }
+          {dogs.map((dog, i) =>
+            dog[0] !== dog[0].toUpperCase() ? (
+              <div className="apskritimas" key={i}>
+                {dog}
+              </div>
+            ) : null
+          )}
         </section>
 
         <section>
-          {
-            dogs2.map((dog, i) => <div className="suo" key={i}>
+          {dogs2.map((dog, i) => (
+            <div className="suo" key={i}>
               <div className="kaire-ausis"></div>
               <div className="snukis">
                 <div className="akys">
-                  <div className='rainele'><div className='vizdys'></div></div>
-                  <div className='rainele'><div className='vizdys'></div></div>
+                  <div className="rainele">
+                    <div className="vizdys"></div>
+                  </div>
+                  <div className="rainele">
+                    <div className="vizdys"></div>
+                  </div>
                 </div>
                 <p>{dog}</p>
               </div>
               <div className="desine-ausis"></div>
-            </div>)
-          }
+            </div>
+          ))}
         </section>
         {/* Naudojant masyvą dogs atvaizduoti skaičius, kurie yra lygūs žodžių masyve ilgiui. Skaičius, didesnius nei 6 atvaizduoti žaliai, kitus raudonai. */}
         <section>
-          {
-            dogs.map((dog, i) => <div key={i} className='number' style={{color:dog.length > 6 ? "green" : "red", boxShadow:dog.length > 6 ? "0 0 15px green" :"0 0 15px red" }}>{dog.length}</div>)
-          }
+          {dogs.map((dog, i) => (
+            <div key={i} className={dog.length > 6 ? 'green' : 'red'}>
+              {dog.length}
+            </div>
+          ))}
         </section>
       </header>
     </div>
